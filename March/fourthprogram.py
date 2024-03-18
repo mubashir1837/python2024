81f13601ac19415896bee5c0a8c18ee5df5316d1
My Name <someone@email.com>
2024-03-19 04:42:19 +0500

    added mathematical operation

---
+++ March/fourthprogram.py
@@ -0,0 +1,214 @@
+print("My name is Mubashir.","My age is 23")
+
+
+print(45)
+print(65)
+
+name="Mubashir"
+age=21
+height=5.43
+hobby= "Programming"
+
+print("my name is :", name)
+print("my age is :", age)
+print("my height is :", height)
+print("my hobby is :", hobby)
+
+
+
+print("name")
+print(name)
+print("age")
+print(age)
+print("height")
+print(height)
+print("hobby")
+print(hobby)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(hobby))
+
+
+name1="sk"
+name2="""sk"""
+name3=""""sk"""
+
+
+age=23
+old=False
+a=None
+print(type(age))
+print(type(old))
+print(type(a))
+
+
+
+a=2
+b=3
+sum = a + b
+print(sum)
+
+a = 12
+b = 3
+diff=a-b
+print(diff)
+
+
+##"Now learn comments in python"
+
+
+
+print("hello world")
+"""
+Multi=line
+comment
+"""
+
+
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+# print("hello world")
+# print("hello world")
+# print("hello world")
+# print("hello world")
+
+# For direct comment;(contol+ slash),"/"
+
+
+
+## TYPES OF OPERATORS:
+# Arithmetic operators(+,-,*,%,**)
+a = 4
+b = 3
+# Then
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a **b)
+
+print(type(a + b))
+print(type(a - b))
+print(type(a * b))
+print(type(a / b))
+print(type(a % b)) # Modulo is used to find remainder
+print(type(a ** b)) #a^b can be find by : **
+
+#Relational or Comparison Operators:(==,!=,>,<,>=,<=)
+a = 20
+b = 10
+
+print(a == b) # Answer False
+print(a != b) # Answer True
+print(a >= b) # Answer True
+print(a > b) # Answer True
+print(a <= b) # Answer False
+print(a < b) # Answer False
+
+
+# For Example
+a = 50
+b= 20
+
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a > b)
+print(a <= b)
+print(a < b)
+
+
+# ASSIGNMENT OPERATORS:(=,+=,-=,*=,/=,%=,**=)
+
+num = 10
+# num = num + 10
+#OR 
+num += 10
+print("num :", num)
+
+# As
+
+num = 20
+#num = num - 20
+#OR
+num -= 20
+print("num :",num)
+#as
+num = 10
+num *= 5
+print("num :", num)
+
+num = 18
+num /= 10
+print("num :", num)
+print(type(num))
+
+
+num = 50
+num %= 45
+print("num :", num )
+
+
+num = 2
+ans=num**5
+print("num :", ans) # 2^5
+
+
+a=6
+b=a**5
+print("ans :",b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
