64b5d611c314110a74ccabc38876917e6282c0dd
My Name <someone@email.com>
2024-03-31 00:38:52 +0500

    Set in Python

---
+++ March/Loops.py
@@ -0,0 +1,51 @@
+### Loops in Python: Loops are used to repeat instructions.
+##Types of Loops:
+#whil(e Loops and for loops
+
+count=1
+while  count<=5:
+    print("mubashir")
+    count+=1
+print(count)
+
+
+num =1
+while num<=7:
+    print("Hello World")
+    num+=1
+
+
+google=1
+while google<=9:
+    print("GOOGLE")
+    google+=1
+print(google)
+
+
+Facebook =-1
+while Facebook<=6:
+    print("instagram")
+    Facebook+=1
+print(Facebook)
+
+
+whatsapp=1
+while whatsapp<=100:
+    print("I love python", whatsapp)
+    whatsapp+=1
+print(whatsapp)
+
+
+i=1
+while i <= 1000:
+    print("UCI",i)
+    i+=1
+
+
+
+
+
+
+
+
+
