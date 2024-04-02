a2bcfd60b7c5d340d02c0764f7ab0ae83e99c8f7
My Name <someone@email.com>
2024-04-03 04:53:39 +0500

    Functions in Python

---
+++ March/break&continue.py
@@ -0,0 +1,33 @@
+i = 1
+while 1 <=5:
+    print(i)
+    if(i==3):
+        break
+    i+=1
+
+i = 0
+while i <=5:
+    if(i ==3):
+      i += 1
+      continue
+    print(i)
+    i += 1
+
+nums =[1,2,3,4,5]
+
+for val in nums:
+    print(val)
+
+tuple=(7,8,6,9,5)
+for val in tuple:
+    print(val)
+
+
+str = "mubashialidddddddddddfrgsj"
+for char in str:
+    print(char)
+
+
+num=[1,4,7,34,6,8,97,6,]
+for el in num:
+    print(el)
