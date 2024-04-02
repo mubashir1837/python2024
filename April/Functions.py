9ebe0e462ead12f80f430b70a6ddf9519f4fb364
My Name <someone@email.com>
2024-04-02 07:19:56 +0500

    Functions in Python

---
+++ April/Functions.py
@@ -0,0 +1,202 @@
+def calc_sum(a,b):
+    sum =a+b
+    print(sum)
+    return sum
+
+calc_sum(2,6)
+calc_sum(5,4)
+
+def cal_sum(a, b):
+    return a+b
+sum =calc_sum(120, 62)
+print(sum)
+
+def print_hello():
+    print("hello")
+print_hello()
+print_hello()
+print_hello()
+print_hello()
+print_hello()
+print_hello()
+print_hello()   
+
+
+# average of 3numbers
+def calc_avg(a,b,c):
+    sum = a+b+c
+    avg=sum / 3
+    print(avg)
+    return(avg)
+
+calc_avg(1,2,3)
+calc_avg(5,7,9)
+calc_avg(3,4,7)
+
+print("mubashir",end=" ")
+print("ali")
+
+
+def calc_avg(a,b,c):
+    avg=sum / 3
+    print(avg)
+    return(avg)
+calc_avg(2,4,6)
+calc_avg(3,9,7)
+calc_avg(5,1,3)
+
+
+
+def calc_avg(a,b):
+    avg=sum / 2
+    print(avg)
+    return(avg)
+
+calc_avg(4,4)
+
+
+def calc_sum(a, b):
+    sum = a + b
+    print(sum)
+    return(sum)
+
+calc_sum(3,4)
+calc_sum(100, 400)
+calc_sum(4, 88)
+calc_sum(30000000, 40000000)
+calc_sum(5677, 5644)
+
+
+def calc_avg(a, b, c):
+    sum=a+b+c
+    avg = sum/3
+    print(avg)
+    return(avg)
+
+calc_avg(5, 8, 8)
+calc_avg(6, 4, 6)
+
+
+def print_mubashir():
+    print("mubashir")
+
+print_mubashir()
+print_mubashir()
+print_mubashir()
+print_mubashir()
+print_mubashir()
+
+
+
+def print_google():
+    print("google")
+
+
+print_google()
+print_google()
+print_google()
+print_google()
+print_google()
+print_google()
+
+
+def print_facebook():
+    print("facebook")
+
+print_facebook()
+print_facebook()
+print_facebook()
+print_facebook()
+print_facebook()
+print_facebook()
+print_facebook()
+print_facebook()
+print_facebook()
+print_facebook()
+print_facebook()
+print_facebook()
+print_facebook()
+print_facebook()
+print_facebook()
+print_facebook()
+print_facebook()
+print_facebook()
+print_facebook()
+print_facebook()
+print_facebook()
+print_facebook()
+print_facebook()
+
+
+def calc_avg(a, b, c):
+    sum = a + b + c
+    avg = sum/3
+    print(avg)
+    return(avg)
+
+calc_avg(4, 6, 7)
+calc_avg(1, 1, 1)
+calc_avg(5, 6, 8)
+calc_avg(1, 5, 6)
+calc_avg(8, 9, 3)
+
+cities = ["Karachi", "Islamabad", "Rawalpindi", "Skardu"]
+countries =["Pakistan", "India", "China", "Bangladesh"]
+
+def print_len(list):
+    print(len(list))
+
+print_len(cities)
+print_len(countries)
+
+# Print Functions
+print("google", end=" ")
+print("chrome")
+
+print("facebook", end=" & ")
+print("instagram")
+
+print("facebook", end=" $ ")
+print("instagram")
+
+
+print("youtube", end=" 7 ")
+print("vidmate")
+
+
+print("Hello", end=" Hi ")
+print("How are you?")
+
+
+def  calc_prod(a=1,b=1):
+    print(a*b)
+    return a*b
+
+calc_prod()
+
+
+n = 5
+fact =1
+for i in range(1, n+1):
+    fact *= i
+print(fact)
+
+
+n = 6
+def calc_fact(n):
+    fact = 1
+    for i in range(1, n+1):
+     fact *= i
+print(fact)
+
+calc_fact(7)
+
+
+def converter(usd_val):
+    pkr_val =usd_val * 280
+    print(usd_val, "USD =", pkr_val, "PKR")
+
+converter(5)
+converter(7)
+converter(59)
+converter(1000)
---
+++ April/forloop.py
@@ -0,0 +1,33 @@
+seq=range(5)
+print(seq[0])
+print(seq[1])
+print(seq[2])
+print(seq[3])
+
+seq=range(10)
+for i in seq:
+    print(i)
+
+for i in range(2,100,2):
+    print(i)
+for i in range(1,101):
+    print(i)
+
+for i in range(100,0,-1):
+    print(i)
+
+
+name="mubashir"
+for i in name:
+    print(i)
+
+colors =["red","green","blue","yellow"]
+for  color in colors:
+    print(color)
+    for i in color:
+        print(i)
+
+        
+for k in range(1, 20001):
+    print(k)
+
