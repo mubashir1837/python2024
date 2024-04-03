f489129608935905e759af8c642b6dcd16fa22c3
My Name <someone@email.com>
2024-04-03 06:27:58 +0500

    recursions in python

---
+++ April/recursion.py
@@ -0,0 +1,27 @@
+def show(n):
+    if (n == 0):
+        return
+    print(n)
+    show(n-1)
+print(5)
+
+
+
+def fact(n):
+    if(n == 1 or n == 0):
+        return 1
+    return fact(n-1) *n
+
+print(fact(2))
+print(fact(4))
+
+
+
+def print_list(list, idx=0):
+    if(idx == len(list)):
+        return
+    print(list[idx])
+    print_list(list, idx+1)
+
+    fruits =["mango","litchi", "apple", "banana"]
+    print_list(fruits)
