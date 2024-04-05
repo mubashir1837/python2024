db1df74664ad3ba8404808812e45f1a1eb51bb31
My Name <someone@email.com>
2024-04-06 01:29:06 +0500

    File

--- April/File_input.py
+++ April/File_input.py
@@ -23,6 +23,33 @@ f.close()
 f = open("demo.txt", "a") #append
 f.write("Then I")
 
+f = open("demo.txt", "r+")
+f.write("abc")
+f.close()
 
+
+#with Syntax
+
+import os
+os.remove("demo.txt")
+
+with open("practice.txt","w") as f:
+    f.write("Hi every\n am learning file")
+    f.write("Using Python\n I love programming")
+
+
+with open("practice.txt", "r") as f:
+    data =f.read()
+
+new_data = data.replace("Python", "Java")
+print(new_data)
+
+word = "love"
+with open("practice.txt","r") as f:
+    data =f.read()
+    if(data.find(word)) != -1:
+        print("Found")
+    else:
+        print("not found")
 
 
