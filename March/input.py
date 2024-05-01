ed52cb74e385347667f4190fb8ef2c0a5b8033ad
My Name <someone@email.com>
2024-05-02 00:00:02 +0500

    Python

--- March/input.py
+++ March/input.py
@@ -22,3 +22,139 @@ a = int(input("enter first : "))
 b = int(input("enter second: "))
 
 print ("avg=",(a+b)/2)
+
+
+a =int(input("Enter your age"))
+print("Your age is ", a)
+
+if(a>18):
+  print("You can drive")
+
+else:
+  print("You cannot drive")
+  
+b =int(input("Enter your marks"))
+print("Your marks is", b)
+
+if (b>50):
+   print("You are pass")
+
+else:
+  print("You are fail")
+
+c =int(input("Enter your score"))
+print("Your score is", c)
+
+if (c>90):
+  print("You are excellent")
+
+else:
+  print("You are not excellent")
+
+d =int(input("Enter your marks:"))
+print("Your marks is", d)
+
+if (d>70):
+  print("You are good")
+  
+else:
+   print("You are fail")
+
+
+marks =int(input("Enter your marks"))
+print("Your marls is ", marks)
+
+if (marks > 90):
+  print("Your are good")
+
+else:
+  print("Your are not excellent")
+
+
+
+
+d =int(input("Enter your marks"))
+print("Your marks is", d)
+
+if (d> 70):
+  print("You are intelligent")
+
+else:
+  print("You are not intelligent")
+
+
+e =int(input("enter Your points"))
+print("Your points are:", e)
+
+if (e > 80):
+  print("Your point is good ")
+
+else:
+  print("Your point is not good")
+
+
+
+f = int(input("Enter your marks"))
+print("Your marks is", f)
+
+print(f>18)
+print(f<18)
+print(f>= 18)
+print(f<=18)
+print(f!=18)
+
+if (f>18):
+  print("You can vote")
+
+else:
+  print("You cannot vote")
+
+
+g =int(input("Enter you age"))
+print("Your age is", g)
+
+if (g>18):
+  print("Bhalig")
+
+else:
+  print("Na bhalig")
+
+h =int(input("Enter your age"))
+print("Your age is", h)
+
+if (h>18):
+  print("Mature")
+
+else:
+  print("Not mature")
+
+
+
+apple = 210
+budget = 200
+if (apple <= budget):
+  print("Mubashir add 1 kg apple to the cart")
+
+else:
+  print("Mubashir do not add apple to the cart")
+
+num = int(input("Enter the value of num:"))
+if (num < 0):
+  print("Number is negative")
+elif (num == 0):
+  print("Number is zero")
+else:
+  print("Number is positive")
+
+  
+  
+
+
+
+
+
+
+
+
+
+
